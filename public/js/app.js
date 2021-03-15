b977c0dc1915be3e9c00a08ff194f82f2c60fd57
ebenezer <e.kankam95@hotmail.com>
2021-03-15 10:55:07 -0500

    adjusted title size

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
-
+const links = document.querySelector('.links');
+const navBar = document.querySelector('#nav');
 
 const showNavbar = () => {
     linksContainer.classList.toggle('show');
 };
+
+// change navbar on scroll
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navBarHeight = navBar.getBoundingClientRect().height;
+    if (scrollHeight > navBarHeight) {
+        navBar.classList.add('nav-fixed');
+    } else {
+        navBar.classList.remove('nav-fixed');
+    }
+});
+
 navToggle.addEventListener('click', showNavbar);
