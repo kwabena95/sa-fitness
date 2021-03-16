855405e4ced7604c06c265bc3bc923f8fa0a20bb
ebenezer <e.kankam95@hotmail.com>
2021-03-16 11:30:00 -0500

    added javascript functionality for sliding to the next video. Replaced dummy text with client's essay about their self

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,10 @@ const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 const navBar = document.querySelector('#nav');
+const prevBtn = document.querySelector('.btn-prev');
+const nextBtn = document.querySelector('.btn-next');
+const slides = document.querySelectorAll('.slide');
+
 
 const showNavbar = () => {
     linksContainer.classList.toggle('show');
@@ -16,6 +20,38 @@ window.addEventListener('scroll', () => {
     } else {
         navBar.classList.remove('nav-fixed');
     }
+
 });
 
+// video slider
+const nextSlide = () => {
+    // get current slide element
+    const current = document.querySelector('.current');
+    // remove current class from slide
+    current.classList.remove('current');
+    if (current.nextElementSibling) {
+        // add to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        // add current to first slide
+        slides[0].classList.add('current');
+    }
+}
+
+const prevSlide = () => {
+    // get current slide element
+    const current = document.querySelector('.current');
+    // remove current class from slide
+    current.classList.remove('current');
+    if (current.previousElementSibling) {
+        // add to next sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        // add current to first slide
+        slides[slides.length - 1].classList.add('current');
+    }
+}
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
 navToggle.addEventListener('click', showNavbar);
